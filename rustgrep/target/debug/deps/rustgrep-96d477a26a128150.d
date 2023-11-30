0c3b631ebf60118ce925f7f0c27810e9b38f25e0
Zach Bochanski <ztbochanski@gmail.com>
2023-11-30 15:57:39 -0800

    add new rust project

---
+++ rustgrep/target/debug/deps/rustgrep-96d477a26a128150.d
@@ -0,0 +1,5 @@
+/Users/zachbochanski/Desktop/rust-grep-utility/rustgrep/target/debug/deps/rustgrep-96d477a26a128150: src/main.rs
+
+/Users/zachbochanski/Desktop/rust-grep-utility/rustgrep/target/debug/deps/rustgrep-96d477a26a128150.d: src/main.rs
+
+src/main.rs:
---
+++ rustgrep/target/debug/rustgrep.d
@@ -0,0 +1,1 @@
+/Users/zachbochanski/Desktop/rust-grep-utility/rustgrep/target/debug/rustgrep: /Users/zachbochanski/Desktop/rust-grep-utility/rustgrep/src/main.rs
