b15787dba8d460be8acc5ba86258f0baa633654a
Zach Bochanski <ztbochanski@gmail.com>
2023-11-30 17:36:24 -0800

    refacttor Config build to use unwrap
    if the Err enum is returned in the Result then the closure is executed

--- rustgrep/src/main.rs
+++ rustgrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
+use std::process;
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args);
+    let config = Config::build(&args).unwrap_or_else(|err|  {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
