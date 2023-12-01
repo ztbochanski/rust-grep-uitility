4f1f59f0a2af2f9dd194418fc00a4106a172e090
Zach Bochanski <ztbochanski@gmail.com>
2023-11-30 16:34:55 -0800

    refactor config to use a struct instead of using a tuple
    this makes the code easier to understand the purpose

--- rustgrep/src/main.rs
+++ rustgrep/src/main.rs
@@ -3,21 +3,26 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-     
-    let (query, file_path) = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(file_path)
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String])  -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String])  -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
