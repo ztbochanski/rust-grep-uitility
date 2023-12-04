9d010387e52311c677632d007640db06a095caff
Zach Bochanski <ztbochanski@gmail.com>
2023-12-04 12:52:55 -0800

    refactor main to use a crate, so we can divide responsibilities
    fewer responsibilities in main for easier testing and better readability

--- rustgrep/src/main.rs
+++ rustgrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
 use std::process;
-use std::fs;
+
+use rustgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,33 +14,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = rustgrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }  
-}
-
-fn run (config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String])  -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone(); // clone is expensive but simple
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
 }
