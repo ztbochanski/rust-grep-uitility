666029a287795195bb4a969719741ed2d744446e
Zach Bochanski <ztbochanski@gmail.com>
2023-12-04 16:40:17 -0800

    remove println macros from lib and  main
    no longer needed upon start of TDD refactor

--- rustgrep/src/main.rs
+++ rustgrep/src/main.rs
@@ -11,11 +11,7 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = rustgrep::run(config) {
-        println!("Application error: {e}");
         process::exit(1);
     }  
 }
