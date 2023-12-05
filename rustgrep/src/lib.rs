c675fb2e42eedc613f97b56f09302d932186ea6b
Zach Bochanski <ztbochanski@gmail.com>
2023-12-04 18:12:07 -0800

    add case insensitive search and test

--- rustgrep/src/lib.rs
+++ rustgrep/src/lib.rs
@@ -33,14 +33,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
-        let query = "ductiv";
+    fn case_sensitive() {
+        let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."], 
+            search_case_insensitive(query, contents)
+        );
     }
 }
 
@@ -50,6 +66,22 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     for line in contents.lines() {
         if line.contains(query) {
             results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line)
         }
     }
 
